a94951123bcea0c78fc93abb36e4aab44ee1dcb4
Anthony Carbajal <acarbajal721@live.com>
2019-07-01 23:22:18 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,14 +2,12 @@ def first_challenge
    epic_tragedy = {
   :montague => {
     :patriarch => {
-      :name => "Lord Montague",
-      :age => "53"
+      
     }
   },
   :capulet => {
     :patriarch => {
-      :name => "Lord Capulet",
-      :age => "50"
+      
     }
   }
 }
